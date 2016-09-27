0f7b24261c5acbb2c90ddb70e339eb150005ab46
daemma <daemma@riseup.net>
2016-09-26 18:38:21 -0600

    Add rainbowstream (twitter) helpers

--- shell/.shell.d/software/rainbowstream.sh
+++ shell/.shell.d/software/rainbowstream.sh
@@ -7,7 +7,7 @@
 ##! @copyright  GPLv3+
 
 TWITTER="${TWITTER:-$(get_which rainbowstream)}"
-if [ -x "$GPP" ] ; then
+if [ -x "${TWITTER}" ] ; then
     export TWITTER="${TWITTER}"
     alias twitter='${TWITTER} --proxy-host localhost --proxy-port 9050 --proxy-type SOCKS5'
 else

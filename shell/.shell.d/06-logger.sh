89836ff3bd315f41e943577cf76a96d7eb99c73a
daemma <daemma@riseup.net>
2016-11-18 08:11:49 -0500

    Adjust logging methods

--- shell/.shell.d/06-logger.sh
+++ shell/.shell.d/06-logger.sh
@@ -8,7 +8,7 @@
 ##  See: https://github.com/kvz/bash3boilerplate/blob/master/main.sh
 
 export LOG_LEVEL="${LOG_LEVEL:-5}"     ## 6=debug ... 0=fatal
-export LOG_NOCOLOR="${LOG_NOCOLOR:-}"  ## true = disable color, otherwise autodetected
+export LOG_NOCOLOR="${LOG_NOCOLOR:-}"  ## true = no color, else autodetected
 
 function _fmt () {
   local color_reset="\x1b[0m"
@@ -22,20 +22,37 @@ function _fmt () {
   local colorvar=color_$1
 
   local color="${!colorvar:-$color_error}"
-  if [ "${LOG_NOCOLOR}" = "true" ] || [[ "${TERM:-}" != "xterm"* ]] || [ -t 1 ]; then
+  if [ "${LOG_NOCOLOR}" = "true" ] || [[ "${TERM:-}" != "xterm"* ]] || [ -t 1 ]
+  then
     # Don't use colors on pipes or non-recognized terminals
     color=""; color_reset=""
   fi
-  echo -e "$(date -u +"%Y-%m-%d %H:%M:%S UTC") ${color}$(printf "[%7s]" ${1})${color_reset}";
+  echo -n "$(date -u +"%Y-%m-%d %H:%M:%S UTC") "
+  echo -e "${color}$(printf "[%7s]" ${1})${color_reset}"
 }
 
-function fatal () {                                 echo "$(_fmt fatal) ${@}" 1>&2 || true; } #exit 1; }
-function alert ()     { [ "${LOG_LEVEL}" -ge 1 ] && echo "$(_fmt alert) ${@}" 1>&2 || true; }
-function error ()     { [ "${LOG_LEVEL}" -ge 2 ] && echo "$(_fmt error) ${@}" 1>&2 || true; }
-function warning ()   { [ "${LOG_LEVEL}" -ge 3 ] && echo "$(_fmt warning) ${@}" 1>&2 || true; }
-function notice ()    { [ "${LOG_LEVEL}" -ge 4 ] && echo "$(_fmt notice) ${@}" 1>&2 || true; }
-function info ()      { [ "${LOG_LEVEL}" -ge 5 ] && echo "$(_fmt info) ${@}" 1>&2 || true; }
-function debug ()     { [ "${LOG_LEVEL}" -ge 6 ] && echo "$(_fmt debug) ${@}" 1>&2 || true; }
+function fatal () {
+    echo "$(_fmt fatal) ${@}" 1>&2 || true;
+    #exit 1;
+}
+function alert () {
+    [ "${LOG_LEVEL}" -ge 1 ] && echo "$(_fmt alert) ${@}" 1>&2 || true;
+}
+function error () {
+    [ "${LOG_LEVEL}" -ge 2 ] && echo "$(_fmt error) ${@}" 1>&2 || true;
+}
+function warning () {
+    [ "${LOG_LEVEL}" -ge 3 ] && echo "$(_fmt warning) ${@}" 1>&2 || true;
+}
+function notice () {
+    [ "${LOG_LEVEL}" -ge 4 ] && echo "$(_fmt notice) ${@}" 1>&2 || true;
+}
+function info () {
+    [ "${LOG_LEVEL}" -ge 5 ] && echo "$(_fmt info) ${@}" 1>&2 || true;
+}
+function debug () {
+    [ "${LOG_LEVEL}" -ge 6 ] && echo "$(_fmt debug) ${@}" 1>&2 || true;
+}
 
 function print_log_examples(){
     debug   "Info useful to developers."
@@ -44,7 +61,7 @@ function print_log_examples(){
     warning "Warning messages, but not an error"
     error   "Non-urgent failures."
     alert   "Should be corrected immediately."
-    fatal   "A \"panic\" condition usually affecting multiple apps/servers/sites."
+    fatal   "A \"panic\" condition usually affecting multiple things."
 }
 # print_log_examples
 
